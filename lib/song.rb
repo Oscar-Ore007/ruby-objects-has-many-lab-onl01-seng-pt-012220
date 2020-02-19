f122acf567830d27f1d68bdf9808f77acc6c3530
Oscar Ore <oscar.ore2011@gmail.com>
2020-02-19 04:03:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,16 @@
 class Song
-  attr_accessor :name, :artist
-
-  def initialize(name)
-    @name = name
-  end
-
-  def artist_name
-    if @artist == nil
-      return nil
-    else
-      @artist.name
+    attr_accessor :artist, :name
+  
+    def initialize(name)
+      @name = name
     end
-  end
-end
+  
+    def artist_name
+      if artist
+        self.artist.name
+      else
+        nil
+      end
+    end
+  
+  end 
